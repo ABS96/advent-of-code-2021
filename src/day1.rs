4ae9a279ea958ef1593dee5d2613422df2913e77
ABS <antok.benedek@gmail.com>
2021-12-01 16:21:49 +0100

    solve day1, part2

--- src/day1.rs
+++ src/day1.rs
@@ -12,7 +12,7 @@ pub fn input_generator(input: &str) -> Vec<u32> {
 }
 
 #[aoc(day1, part1)]
-pub fn solve_part1(depths: &[u32]) -> u32 {
+pub fn count_increases(depths: &[u32]) -> u32 {
   let mut last_depth: Option<u32> = None;
   let mut increases: u32 = 0;
 
@@ -34,4 +34,21 @@ pub fn solve_part1(depths: &[u32]) -> u32 {
   }
 
   increases
+}
+
+#[aoc(day1, part2)]
+pub fn depth_sliding_window(depths: &[u32]) -> u32 {
+  let mut window: [u32; 3] = [0; 3];
+  let mut w_cur: usize = 0;
+  let mut sums: Vec<u32> = Vec::new();
+
+  for (idx, depth) in depths[..].iter().enumerate() {
+    window[w_cur] = *depth;
+    // Keep cursor going around on window array
+    w_cur = (w_cur + 1) % 3;
+    if idx > 2 {
+      sums.push(window.iter().sum());
+    }
+  }
+  count_increases(&sums)
 }
