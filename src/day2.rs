53e0303a9058065e7c502f9f0f686db022d2684d
ABS <antok.benedek@gmail.com>
2021-12-02 12:09:00 +0100

    solve day 2, part 1

---
+++ src/day2.rs
@@ -0,0 +1,61 @@
+// use std::fmt;
+
+#[derive(Debug, Copy, Clone)]
+enum Directions {
+  Forward,
+  Down,
+  Up,
+}
+
+// impl fmt::Display for Directions {
+//   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//     match self {
+//       Directions::Forward => write!(f, "Forward"),
+//       Directions::Down => write!(f, "Down"),
+//       Directions::Up => write!(f, "Up"),
+//     }
+//   }
+// }
+
+#[derive(Debug, Copy, Clone)]
+pub struct Command {
+  direction: Directions,
+  distance: u32,
+}
+
+#[aoc_generator(day2)]
+pub fn command_stream(input: &str) -> Vec<Command> {
+  input
+    .lines()
+    .map(|l| {
+      let mut words = l.trim().splitn(2, ' ');
+      let dir_word = words.next().unwrap();
+      Command {
+        direction: match dir_word {
+          "forward" => Directions::Forward,
+          "down" => Directions::Down,
+          "up" => Directions::Up,
+          _ => panic!("Wrong direction: {}", dir_word),
+        },
+        distance: words.next().unwrap().parse::<u32>().unwrap(),
+      }
+    })
+    .inspect(|c| println!("{:?}", c))
+    .collect()
+}
+
+#[aoc(day2, part1)]
+pub fn calc_mul(commands: &Vec<Command>) -> i32 {
+  // Separate horizontal and vertical commands
+  let (hor, ver): (Vec<Command>, Vec<Command>) = commands
+    .iter()
+    .partition(|&c| matches!(c.direction, Directions::Forward));
+  // Calculate total distances
+  let hor: i32 = hor.iter().fold(0, |acc, c| acc + c.distance as i32);
+  let ver: i32 = ver.iter().fold(0, |acc, c| match c.direction {
+    Directions::Up => acc - c.distance as i32,
+    Directions::Down => acc + c.distance as i32,
+    _ => panic!("Wrong direction: {:?}", c.direction),
+  });
+  hor * ver
+}
