ab5ccefd4372ad8a69ea55b3cb6918da274aef28
ABS <antok.benedek@gmail.com>
2021-12-07 14:26:20 +0100

    solve day 7, part 2

--- src/day7.rs
+++ src/day7.rs
@@ -1,27 +1,41 @@
-use std::collections::HashMap;
+#[derive(Copy, Clone, Debug)]
+enum FuelBurnRate {
+  Constant,
+  Increasing,
+}
 
 #[aoc_generator(day7)]
-pub fn sub_positions(input: &str) -> Vec<u32> {
+pub fn subs(input: &str) -> Vec<u32> {
   input.split(",").map(|pos| pos.parse().unwrap()).collect()
 }
 
-#[aoc(day7, part1)]
-pub fn fuel_cost(positions: &Vec<u32>) -> u32 {
-  positions
-    .iter()
-    .fold(
-      (HashMap::new(), u32::MAX),
-      |(mut costs, lowest_cost), pos| {
-        let mut lowest = lowest_cost;
-        if !costs.contains_key(pos) {
-          let total_dist = positions
-            .iter()
-            .fold(0, |t, p| t + (u32::max(*pos, *p) - u32::min(*pos, *p)));
-          costs.insert(*pos, total_dist);
-          lowest = u32::min(lowest_cost, total_dist);
-        }
-        (costs, lowest)
-      },
+fn fuel_cost(x1: u32, x2: u32, rate: FuelBurnRate) -> u32 {
+  let dist = u32::max(x1, x2) - u32::min(x1, x2);
+  match rate {
+    FuelBurnRate::Constant => dist,
+    FuelBurnRate::Increasing => (dist * (dist + 1)) / 2, // triangular number
+  }
+}
+
+fn lowest_cost(subs: &Vec<u32>, rate: FuelBurnRate) -> u32 {
+  // consider all positions between the leftmost and rightmost submarine
+  (0..=*subs.iter().max().unwrap()).fold(u32::MAX, |lowest, pos| {
+    u32::min(
+      // for each position, calculate the cost of all submarines going there
+      lowest,
+      subs
+        .iter()
+        .fold(0, |total, sub| total + fuel_cost(pos as u32, *sub, rate)),
     )
-    .1
+  })
+}
+
+#[aoc(day7, part1)]
+pub fn part1(positions: &Vec<u32>) -> u32 {
+  lowest_cost(positions, FuelBurnRate::Constant)
+}
+
+#[aoc(day7, part2)]
+pub fn part2(positions: &Vec<u32>) -> u32 {
+  lowest_cost(positions, FuelBurnRate::Increasing)
 }
