9df473a6452e1b051ea39eaa7daefe153bc45e81
ABS <antok.benedek@gmail.com>
2022-04-13 23:17:33 +0200

    solve day 12, part 2

--- src/day12.rs
+++ src/day12.rs
@@ -32,14 +32,19 @@ fn traverse(
   node: &str,
   path: Vec<String>,
   routes: &mut Vec<Vec<String>>,
+  twice: Option<&str>,
 ) {
-  let mut p = path.clone();
-  p.push(node.to_string());
+  let mut new_path = path.clone();
+  new_path.push(node.to_string());
   if node == "end" {
-    routes.push(p);
+    routes.push(new_path);
   } else if node.chars().all(|c| c.is_ascii_uppercase()) || !path.contains(&node.to_string()) {
     for neighbour in cave.get(&node).unwrap() {
-      traverse(cave, neighbour, p.clone(), routes);
+      traverse(cave, neighbour, new_path.clone(), routes, twice);
+    }
+  } else if twice.is_none() && node != "start" {
+    for neighbour in cave.get(&node).unwrap() {
+      traverse(cave, neighbour, new_path.clone(), routes, Some(node));
     }
   }
 }
@@ -48,6 +53,14 @@ fn traverse(
 pub fn all_paths(input: &str) -> u32 {
   let cave: HashMap<&str, HashSet<&str>> = map_cave(input);
   let mut routes: Vec<Vec<String>> = Vec::new();
-  traverse(&cave, "start", Vec::new(), &mut routes);
+  traverse(&cave, "start", Vec::new(), &mut routes, Some(""));
+  routes.len() as u32
+}
+
+#[aoc(day12, part2)]
+pub fn all_paths_more_time(input: &str) -> u32 {
+  let cave: HashMap<&str, HashSet<&str>> = map_cave(input);
+  let mut routes: Vec<Vec<String>> = Vec::new();
+  traverse(&cave, "start", Vec::new(), &mut routes, None);
   routes.len() as u32
 }
